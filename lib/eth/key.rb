c5d778f804ffe7d4c26356f8881b7e29b605eaeb
James Maddis <ovdjon001@myuct.ac.za>
2018-02-19 22:31:46 +0200

    Update key.rb
    Uncommented the initilizer method

--- lib/eth/key.rb
+++ lib/eth/key.rb
@@ -47,10 +47,10 @@ module Eth
 
     end
 
-    #def initialize(priv: nil)
-      #@private_key = MoneyTree::PrivateKey.new key: priv
-      #@public_key = MoneyTree::PublicKey.new private_key, compressed: false
-    #end
+    def initialize(priv: nil)
+      @private_key = MoneyTree::PrivateKey.new key: priv
+      @public_key = MoneyTree::PublicKey.new private_key, compressed: false
+    end
 
     def private_hex
       private_key.to_hex
